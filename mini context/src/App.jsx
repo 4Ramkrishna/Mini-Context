5ee58ac65b0605ced78cfb9d2d55c6f2c93255db
4Ramkrishna <gs2110045@sgsitsindore.in>
2024-06-17 22:30:27 +0530

    remaining
    r

--- mini context/src/App.jsx
+++ mini context/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react"
+import UserContextProvider from "./context/UserContext/UserContextProvider"
 
 function App() {
 
   return (
-    <h1>Ramkrishna Patidar</h1>
+    <UserContextProvider>
+      <h1>Ramkrishna Patidar</h1>
+    </UserContextProvider>
   )
 }
 
